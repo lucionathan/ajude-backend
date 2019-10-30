062af567e6b73cf09abf1d5b58b4d911801a266d
lucionathan <lucio.matias@ccc.ufcg.edu.br>
2019-10-30 17:18:07 -0300

    jdk fixed and DTO to login created

---
+++ backend/src/main/java/ufcg/project/DTOs/UserDTO.java
@@ -0,0 +1,20 @@
+package ufcg.project.DTOs;
+
+public class UserDTO {
+
+    private String email;
+    private String password;
+
+    public UserDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
