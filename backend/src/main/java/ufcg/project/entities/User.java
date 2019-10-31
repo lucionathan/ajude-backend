ed28ac0bf29b5bb0d22d5f41911d22e748b422b1
João Felipe <joao.freitas@ccc.ufcg.edu.br>
2019-10-31 11:09:30 -0300

    Update User.java

--- backend/src/main/java/ufcg/project/entities/User.java
+++ backend/src/main/java/ufcg/project/entities/User.java
@@ -11,13 +11,15 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+    private String creditCard;
     private String password;
 
     @JsonCreator
-    public User(String firstName, String lastName, String email, String password) {
+    public User(String firstName, String lastName, String email, String creditCard, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.creditCard = creditCard;
         this.password = password;
     }
 
@@ -32,24 +34,36 @@ public class User {
     public String getLastName() {
         return lastName;
     }
+    
+    public String getCreditCard() {
+        return this.creditCard;
+    }
+    
+    public boolean setCreditCard(creditCard) {
+        this.creditCard = creditCard;
+        return true;
+    }
 
-    public void setLastName(String lastName) {
+    public boolean setLastName(String lastName) {
         this.lastName = lastName;
+        return true;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
+    public boolean setEmail(String email) {
         this.email = email;
+        return true;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public boolean setPassword(String password) {
         this.password = password;
+        return true;
     }
 }
