beec8b36343b3af627b3be8bb35d479b2ae1b15a
lucionathan <lucio.matias@ccc.ufcg.edu.br>
2019-11-17 20:47:34 -0300

    .

--- src/main/java/ufcg/project/controllers/DonationController.java
+++ src/main/java/ufcg/project/controllers/DonationController.java
@@ -1,5 +1,6 @@
 package ufcg.project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,14 +15,20 @@ import javax.servlet.ServletException;
 
 @Controller
 public class DonationController {
+
+    @Autowired
     private JWTService jwtService;
+    @Autowired
     private DonationService donationService;
 
     @PostMapping("/campaign/{shortUrl}/donation")
     public ResponseEntity<Campaign> doDonation(@RequestBody DonationDTOValue donation, @RequestHeader("Authorization") String header, @PathVariable("shortUrl") String idCampaign) throws ServletException {
-
+        System.out.println(donation.getDonatedValue());
+        System.out.println(header);
+        System.out.println(idCampaign);
         if(jwtService.userExists(header)){
             Campaign campaign = donationService.doDonation(donation.getDonatedValue(), header, idCampaign);
+            System.out.println(campaign);
             if(campaign == null) {
                 System.out.println("aaa");
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
